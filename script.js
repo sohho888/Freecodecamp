1b3f3d621ad2b8b44d5a643ebe9e78236ad3c158
Vyacheslav Ponomarev <49794467+sohho888@users.noreply.github.com>
2022-03-20 18:20:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -44,4 +44,19 @@ for (let i = 1; i <= 9; i+=2) {
 
 console.log(myArray);
 
+//////Modify function multiplyAll so that it returns the product of all the numbers in the sub-arrays of arr.
+
+  function multiplyAll(arr) {
+    let product = 1;
+  for(let i = 0; i< arr.length; i++) {
+    for(let j = 0; j < arr[i].length; j++){
+      product = product*arr[i][j]
+    }
+  }
+    return product;
+  }
+
+  
+ console.log(multiplyAll([[1, 2], [3, 4], [5, 6, 7]])); 
+
 
