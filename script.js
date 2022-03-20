89a33b944f3bbe2bb197c576c2c35aff21a16efb
Vyacheslav Ponomarev <49794467+sohho888@users.noreply.github.com>
2022-03-20 22:52:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -59,4 +59,15 @@ console.log(myArray);
   
  console.log(multiplyAll([[1, 2], [3, 4], [5, 6, 7]])); 
 
+//////Write a recursive function, sum(arr, n), that returns the sum of the first n elements of an array arr.
+
+ function sum(arr, n) {
+  if(n <= 0) {
+    return 0;
+  } else {
+    return sum(arr, n - 1) + arr[n - 1];
+  }
+}
+
+
 
