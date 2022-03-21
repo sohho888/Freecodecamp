75444642be680af22f66328479849fae63edf9c2
Vyacheslav Ponomarev <49794467+sohho888@users.noreply.github.com>
2022-03-21 13:46:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -116,3 +116,14 @@ function lookUpProfile(name, prop) {
 lookUpProfile("Akira", "likes");
 
 
+
+///////Use parseInt() in the convertToInteger function so it converts a binary number to an integer and returns it.
+function convertToInteger(str) {
+
+  const a = parseInt(str, 2);
+  return a
+  }
+  
+  console.log(convertToInteger("10011"));
+
+
