dd1ac953a2ae8843e9a78876dff8547f781329bc
Vyacheslav Ponomarev <49794467+sohho888@users.noreply.github.com>
2022-03-22 13:39:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -179,3 +179,14 @@ const sum = (...args) => {
 
 console.log(sum(1,2,3222));
 
+
+
+////////Use Destructuring Assignment with the Rest Parameter to Reassign Array Elements
+
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+  const arr = [...list.splice(2)]; 
+  return arr;
+}
+
+console.log(removeFirstTwo(source));
