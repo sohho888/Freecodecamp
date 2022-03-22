baeff5fd084501626c5062bbd2d1b6aac91ce4b4
Vyacheslav Ponomarev <49794467+sohho888@users.noreply.github.com>
2022-03-22 14:18:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -190,3 +190,24 @@ function removeFirstTwo(list) {
 }
 
 console.log(removeFirstTwo(source));
+
+
+///////Create Strings using Template Literals
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+
+function makeList(arr) {
+  
+  const failureItems = [];
+  for(let i= 0; i <arr.length; i++) {
+    failureItems.push(`<li class="text-warning">${arr[i]}</li>`)
+  }
+
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
+
